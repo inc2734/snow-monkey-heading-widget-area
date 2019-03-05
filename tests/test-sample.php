ccb3ed1397849b61676ee6bd3a5ccc302d34c94e
Takashi Kitajima <inc@2inc.org>
2019-03-05 15:40:59 +0900

    Update tests

--- tests/test-sample.php
+++ tests/test-sample.php
@@ -14,7 +14,12 @@ class SampleTest extends WP_UnitTestCase {
 	 * A single example test.
 	 */
 	function test_sample() {
-		// Replace this with some actual testing code.
-		$this->assertTrue( true );
+		$updater = new \Inc2734\WP_GitHub_Plugin_Updater\Bootstrap(
+			plugin_basename( __FILE__ ),
+			'inc2734',
+			'snow-monkey-heading-widget-area'
+		);
+
+		$this->assertTrue( is_a( $updater, '\Inc2734\WP_GitHub_Plugin_Updater\Bootstrap' ) );
 	}
 }
