bc344a93426b2bf28cfe68438244b574268fef32
Takashi Kitajima <inc@2inc.org>
2019-05-09 15:04:29 +0900

    Fix #2

--- src/js/app.js
+++ src/js/app.js
@@ -3,20 +3,19 @@
 import forEachHtmlNodes from '@inc2734/for-each-html-nodes';
 
 const moveWidgetArea = (widgetArea, firstHeading) => {
-  firstHeading.parentNode.insertBefore(widgetArea, firstHeading);
-  widgetArea.setAttribute('aria-hidden', 'false');
+  if (firstHeading) {
+    firstHeading.parentNode.insertBefore(widgetArea, firstHeading);
+  }
+
+  const ariaHidden = firstHeading ? 'false' : 'true';
+  widgetArea.setAttribute('aria-hidden', ariaHidden);
 };
 
 window.addEventListener(
   'DOMContentLoaded',
   () => {
     const firstHeading = document.querySelector('.c-entry__content > h2');
-    if (! firstHeading) {
-      return;
-    }
-
     const widgetAreas = document.getElementsByClassName('l-heading-widget-area');
-
     forEachHtmlNodes(widgetAreas, (element) => moveWidgetArea(element, firstHeading));
   },
   false
